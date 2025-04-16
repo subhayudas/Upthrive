ae880119ddb66f490d853369038d5f22c7d575e9
subhayudas <subhayudas49@gmail.com>
2025-04-17 00:46:13 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import StarsCanvas from "@/components/main/StarBackground";
+import Navbar from "@/components/main/Navbar";
+import Footer from "@/components/main/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  metadataBase: new URL('https://www.UPthrift.xyz/'),
+  title: {
+    default: "Welcome to UPthrift",
+    template: "We're your secret weapon in the digital arena",
+  },
+  description: "We're your secret weapon in the digital arena",
+  twitter: {
+    card: "summary_large_image",
+  },
+  openGraph: {
+    images: '/opengraph-image.png',
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
+      >
+        <StarsCanvas />
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
+
---
+++ app/page.tsx
@@ -0,0 +1,29 @@
+import Hero from "@/components/main/Hero";
+import Projects from "@/components/main/Projects";
+import Services from "@/components/main/Services";
+import TestimonialSlider from "@/components/main/TestimonialSlider";
+import FAQSection from "@/components/main/FAQSection";
+import ContactForm from "@/components/main/ContactForm";
+import AboutUs from "@/components/main/AboutUs";
+import Approach from "@/components/main/Approach";
+import ChooseUs from "@/components/main/ChooseUs";
+
+
+
+
+export default function Home() {
+  return (
+    <main className="h-full w-full">
+      <div className="flex flex-col">
+        <Hero />
+        <Services />
+        
+        <ChooseUs/>
+        <Approach/>
+        <TestimonialSlider />
+       
+        
+      </div>
+    </main>
+  );
+}
