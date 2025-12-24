d4c7a307174baffd102bb1521f351b72282b86eb
subhayudas <subhayudas49@gmail.com>
2025-12-24 11:31:16 +0530

    pushhh

--- components/main/ZoomParallax.tsx
+++ components/main/ZoomParallax.tsx
@@ -29,7 +29,7 @@ export function ZoomParallax({ images }: ZoomParallaxProps) {
 	const scales = [scale4, scale5, scale6, scale5, scale6, scale8, scale9];
 
 	return (
-		<div ref={container} className="relative h-[300vh]">
+		<div ref={container} className="relative h-[300vh] hidden md:block">
 			<div className="sticky top-0 h-screen overflow-hidden">
 				{images.map(({ src, alt }, index) => {
 					const scale = scales[index % scales.length];
